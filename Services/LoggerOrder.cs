c274979392b818d8c5a8e45c2fbb47b144a5503d
Khaled Yasser <kry752005@gmail.com>
2026-07-14 16:47:31 +0300

    Add files via upload

---
+++ Services/LoggerOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWaveConsole.Services
+{
+    public  class LoggerOrder
+    {//
+        public void Log(string message)
+        {
+            File.AppendAllText("app.log", $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+    }
+}
