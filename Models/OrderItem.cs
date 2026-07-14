9bc9dbfb803adaecacb877f79e115b87a14847a8
Khaled Yasser <kry752005@gmail.com>
2026-07-14 17:01:40 +0300

    Update OrderItem.cs

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,9 +1,16 @@
 namespace NorthWaveConsole.Models
 {
     public class OrderItem
-    {
-        public string ProductName;
-        public decimal Price;
-        public int Qty;
+    {//.
+        public string ProductName { get; private set; }
+        public decimal Price { get; private set; }
+        public int Qty { get; private set; }
+
+        public OrderItem(string productName, decimal price, int qty)
+        {
+            ProductName = productName;
+            Price = price;
+            Qty = qty;
+        }
     }
 }
