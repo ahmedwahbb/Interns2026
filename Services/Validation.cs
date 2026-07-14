2e39e821845169d4998b2b571500660301685be5
Khaled Yasser <kry752005@gmail.com>
2026-07-14 16:56:58 +0300

    Add files via upload

---
+++ Services/Validation.cs
@@ -0,0 +1,27 @@
+﻿using NorthWaveConsole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWaveConsole.Services
+{
+    public class Validation
+    {
+        ///
+        public bool IsValid(Order order)
+        {
+            if (order == null)
+                return false;
+
+            if (string.IsNullOrWhiteSpace(order.CustomerName))
+                return false;
+
+            if (order.ItemsCount == 0)
+                return false;
+
+            return true;
+        }
+    }
+}
